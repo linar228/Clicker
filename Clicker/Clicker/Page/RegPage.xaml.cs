c65185021143859afe3c2705409c0771f26bed4d
линар валиев <linar369@mail.ru>
2022-03-21 12:27:40 +0300

    finish fronted regpage

--- Clicker/Clicker/Page/RegPage.xaml.cs
+++ Clicker/Clicker/Page/RegPage.xaml.cs
@@ -16,5 +16,10 @@ namespace Clicker.Page
         {
             InitializeComponent();
         }
+
+        private async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
     }
 }
